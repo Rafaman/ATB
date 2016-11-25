fe9f396c482f2c4c5b025bc17193467a7f014360
Rafaman <raffaele_masucci@hotmail.it>
2016-11-25 21:29:31 +0100

    Add files via upload

--- ATBckarena.m
+++ ATBckarena.m
@@ -2,49 +2,56 @@ function ATBckarena (arduino)
 
     %camera=webcam('Nome_Webcam');
     %image=snapshot(camera);
-    pixel=600;
+    pixelX=640;
+    pixelY=480;
+    soglia_rumore_grigio=10;
     image=imread('prova.png');
-    red=image(:, :, 1);
-    blue=image(:, :, 2);
-    green=image(:, :, 3);
     grayMin={128, 128, 128};
     grayMax={220, 220, 220};
-    imageLogic=image>soglia_grigio;
-    imageCorrect=bwareaopen(imageLogic, soglia_grigio);    
+    imageLogic=image>soglia_rumore_grigio;
+    imageCorrect=bwareaopen(imageLogic, soglia_rumore_grigio);    
     
-    for a=1:1:pixel
+    for a=1:1:255
         
-        for b=1:1:pixel
+        for b=1:1:255
             
-            for c=1:1:pixel
+            for c=1:1:255
                 
-                if grayMin<image(a, b, c)<grayMax
+                if grayMin<=image(a, b, c)<=grayMax
                     
-                    gray(a, b, c)=true;
+                    gray(a, b, c)=image(a, b, c);
                     
                 end
-               
+                
             end
             
         end
         
     end
     
-    [riga, colonna]=find(gray(1:pixel/2, :));
+    [riga, colonna]=find(gray(1:pixelX/2, :));
     
     xa=round(mean(riga));
     ya=round(mean(colonna));
     
-    [riga2, colonna2]=find(gray(pixel/2:pixel, :));
+    [riga2, colonna2]=find(gray(pixelX/2:pixelX, :));
     
     xb=roun(mean(riga2));
     yb=round(mean(colonna2));
     
-    coefficiente_angolare=sqrt((xa-xb)^2+(ya-yb)^2);
-    alpha=atan(coefficiente_angolare);
+    coefficiente_angolare=yb-ya/xb-xa;
+    angolo=atan(coefficiente_angolare);
     
-    disp(alpha);
+    if angolo>0
+        
+        mode=3;
+        
+    else
+        
+        mode=4;
+        
+    end
     
-    ATBmove(alpha);
+    ATBmove(mode, angolo, distance);
     
 end
